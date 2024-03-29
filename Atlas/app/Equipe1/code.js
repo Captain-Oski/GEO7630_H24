20839d89bee875b786a949a1a6460d1bd55b3ab3
Eliott <80271593+eliottness199@users.noreply.github.com>
2024-03-28 23:51:57 -0400

    Update code.js

--- Atlas/app/Equipe1/code.js
+++ Atlas/app/Equipe1/code.js
@@ -18,9 +18,39 @@ var geolocateControl = new maplibregl.GeolocateControl({
 
 map.addControl(geolocateControl, 'bottom-right'); // ajout du contrôle en bas à droite de la carte
 
+
 /// Création du contrôle d'échelle
 var scale = new maplibregl.ScaleControl({
     unit: 'metric' // utilisation de l'unité métrique
 });
 
 map.addControl(scale); // ajout du contrôle en bas à gauche de la carte
+
+
+/// Zoom et boussole
+var view = new MapView({
+   container: "viewDiv",
+   map: map
+});
+
+var zoom = new Zoom({
+  view: view
+});
+
+var compass = new Compass({
+  view: view
+});
+
+view.ui.add(compass, "top-left");
+
+
+/// Affiche la position de l'utilisateur
+var gl = new GraphicsLayer();
+map.add(gl);
+
+var locateWidget = new Locate({
+  view: view,   // Lie le bouton à la vue
+  graphicsLayer: gl  // Couche à laquelle la localisation est affectée
+});
+
+view.ui.add(locateWidget, "top-right");
